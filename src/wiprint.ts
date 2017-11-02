54425f6f8b287d1daf1fb05e91d5f2fc307038fc
MrTarantula <ryantarantula@gmail.com>
2017-11-02 13:02:48 -0500

    Added conversation history.

--- src/wiprint.ts
+++ src/wiprint.ts
@@ -11,142 +11,279 @@ const client = WITClient.getClient();
 const fields: Models.WorkItemField[] = [];
 
 interface IQuery {
-    id: string;
-    isPublic: boolean;
-    name: string;
-    path: string;
-    wiql: string;
+  id: string;
+  isPublic: boolean;
+  name: string;
+  path: string;
+  wiql: string;
 }
 
 interface IActionContext {
-    id?: number;
-    workItemId?: number;
-    query?: IQuery;
-    queryText?: string;
-    ids?: number[];
-    workItemIds?: number[];
-    columns?: string[];
+  id?: number;
+  workItemId?: number;
+  query?: IQuery;
+  queryText?: string;
+  ids?: number[];
+  workItemIds?: number[];
+  columns?: string[];
 }
 
 const dummy = [
-    { name: "Assigned To", referenceName: "System.AssignedTo" },
-    { name: "State", referenceName: "System.State" },
-    { name: "Created Date", referenceName: "System.CreatedDate" },
-    { name: "Description", referenceName: "System.Description" },
-    { name: "Acceptance Criteria", referenceName: "Microsoft.VSTS.Common.AcceptanceCriteria" },
-    { name: "History", referenceName: "System.History" }
+  { name: "Assigned To", referenceName: "System.AssignedTo" },
+  { name: "State", referenceName: "System.State" },
+  { name: "Created Date", referenceName: "System.CreatedDate" },
+  { name: "Description", referenceName: "System.Description" },
+  {
+    name: "Acceptance Criteria",
+    referenceName: "Microsoft.VSTS.Common.AcceptanceCriteria"
+  },
+  { name: "History", referenceName: "System.History" }
 ];
 
 // Utilities
 declare global {
-    interface String {
-        sanitize(): string;
-    }
+  interface String {
+    sanitize(): string;
+    htmlize(): string;
+  }
 }
 
-String.prototype.sanitize = function (this: string) {
-    return this.replace(/\s/g, "-").replace(/[^a-z0-9\-]/gi, "");
+const localeTime = "L LT";
+
+String.prototype.sanitize = function(this: string) {
+  return this.replace(/\s/g, "-").replace(/[^a-z0-9\-]/gi, "");
+};
+
+String.prototype.htmlize = function(this: string) {
+  return this.replace(/<\/*(step|param|desc|comp)(.*?)>/g, "")
+    .replace(/&lt;/g, "<")
+    .replace(/&gt;/g, ">")
+    .replace(/&quot;/g, `"`)
+    .replace(/&apos;/g, "'")
+    .replace(/&amp;/g, "&");
 };
 
 const printWorkItems = {
-    getMenuItems: (context: any) => {
-        let menuItemText = "Print";
-        if (context.workItemIds && context.workItemIds.length > 1) {
-            menuItemText = "Print Selection";
-        }
+  getMenuItems: (context: any) => {
+    let menuItemText = "Print";
+    if (context.workItemIds && context.workItemIds.length > 1) {
+      menuItemText = "Print Selection";
+    }
 
-        return [{
-            action: (actionContext: IActionContext) => {
-                const wids = actionContext.workItemIds || actionContext.ids || [actionContext.workItemId || actionContext.id];
+    return [
+      {
+        action: (actionContext: IActionContext) => {
+          const wids = actionContext.workItemIds ||
+            actionContext.ids || [actionContext.workItemId || actionContext.id];
 
-                return client.getWorkItems(wids, undefined, undefined, Models.WorkItemExpand.Fields)
-                    .then((workItems) => print(workItems));
-            },
-            icon: "static/img/print14.png",
-            text: menuItemText,
-            title: menuItemText,
-        } as IContributedMenuItem];
-    },
+          return getWorkItems(wids)
+            .then(workItems => prepare(workItems))
+            .then(pages => {
+              return Q.all(pages);
+            })
+            .then(pages => {
+              pages.forEach(
+                page => (document.getElementById("workitems").innerHTML += page)
+              );
+
+              window.focus(); // needed for IE
+              let ieprint = document.execCommand("print", false, null);
+              if (!ieprint) {
+                window.print();
+              }
+              document.getElementById("workitems").innerHTML = "";
+            });
+        },
+        icon: "static/img/print14.png",
+        text: menuItemText,
+        title: menuItemText
+      } as IContributedMenuItem
+    ];
+  }
 };
 
 const printQueryToolbar = {
-    getMenuItems: (context: any) => {
-        return [{
-            action: (actionContext: IActionContext) => {
-                return client.queryByWiql(
-                    { query: actionContext.query.wiql }, vssContext.project.name, vssContext.team.name,
-                ).then((result) => {
-                    if (result.workItemRelations) {
-                        return result.workItemRelations.map((wi) => wi.target.id);
-                    } else {
-                        return result.workItems.map((wi) => wi.id);
-                    }
-                }).then((wids) => {
-                    return client.getWorkItems(wids, undefined, undefined, Models.WorkItemExpand.Fields)
-                        .then((workItems) => print(workItems));
+  getMenuItems: (context: any) => {
+    return [
+      {
+        action: (actionContext: IActionContext) => {
+          return client
+            .queryByWiql(
+              { query: actionContext.query.wiql },
+              vssContext.project.name,
+              vssContext.team.name
+            )
+            .then(result => {
+              if (result.workItemRelations) {
+                return result.workItemRelations.map(wi => wi.target.id);
+              } else {
+                return result.workItems.map(wi => wi.id);
+              }
+            })
+            .then(wids => {
+              return getWorkItems(wids)
+                .then(workItems => prepare(workItems))
+                .then(pages => {
+                  return Q.all(pages);
+                })
+                .then(pages => {
+                  pages.forEach(
+                    page =>
+                      (document.getElementById("workitems").innerHTML += page)
+                  );
+
+                  window.focus(); // needed for IE
+                  let ieprint = document.execCommand("print", false, null);
+                  if (!ieprint) {
+                    window.print();
+                  }
+                  document.getElementById("workitems").innerHTML = "";
                 });
-            },
-            icon: "static/img/print16.png",
-            text: "Print All",
-            title: "Print All",
-        } as IContributedMenuItem];
-    },
+            });
+        },
+        icon: "static/img/print16.png",
+        text: "Print All",
+        title: "Print All"
+      } as IContributedMenuItem
+    ];
+  }
 };
 
-function prepare(workItems: Models.WorkItem[]) {
-    return dataService.then((service: IExtensionDataService) => {
-        return workItems.map((item) => {
-            return service.getValue(`wiprint-${item.fields["System.WorkItemType"].sanitize()}`,
-                { scopeType: "user", defaultValue: dummy as Models.WorkItemTypeFieldInstance[] })
-                .then((data: Models.WorkItemTypeFieldInstance[]) => {
-                    // data = data.length == 0 ? dummy as Models.WorkItemTypeFieldInstance[] : data;
-                    let insertText =
-                        `<div class="item"><h2>${item.fields["System.WorkItemType"]} ` +
-                        `${item.id} - ${item.fields["System.Title"]}</h2>`;
-                    data.forEach((field) => {
-                        if (item.fields[field.referenceName]) {
-                            if (moment(item.fields[field.referenceName], "YYYY-MM-DDTHH:mm:ss.SSSZ", true).isValid()) {
-                                insertText += `<p><b>${field.name}:</b> ${moment(item.fields[field.referenceName])
-                                    .format("MM/DD/YYYY")}</p>`;
-                            } else if (item.fields[field.referenceName] instanceof Array) {
-                                insertText += `<p><b>${field.name}:</b></p>`;
-                                item.fields[field.referenceName].forEach((fi) => {
-                                    insertText += `<p>${fi}</p>`;
-                                });
-                            } else {
-                                // dates that didn't pass strict validation above shoule be dropped
-                                // if (!moment(item.fields[field.referenceName], "YYYY-MM-DDTHH:mm:ss.SSSZ").isValid()) {
-                                insertText += `<p><b>${field.name}:</b> ${item.fields[field.referenceName]}</p>`;
-                                // }
-                            }
-                        }
-                    });
-                    insertText += "</div>";
-                    return insertText;
-                });
-        });
-    });
+// Promises
+function getWorkItems(wids: number[]): IPromise<Models.WorkItem[]> {
+  return client.getWorkItems(
+    wids,
+    undefined,
+    undefined,
+    Models.WorkItemExpand.Fields
+  );
 }
 
-function print(workItems: Models.WorkItem[]) {
-    Q.all(prepare(workItems)).then((pages) => {
-        pages.forEach((page) => {
-            Q.resolve(page).then((p) => document.getElementById("workitems").innerHTML += p);
-        });
-    }).then(() => {
-        window.focus(); // needed for IE
-        let ieprint = document.execCommand("print", false, null);
-        if (!ieprint) {
-            window.print();
+function getWorkItemFields() {
+  return client.getFields();
+}
+
+function getFields(workItem: Models.WorkItem) {
+  return dataService.then((service: IExtensionDataService) => {
+    return service
+      .getValue(
+        `wiprint-${workItem.fields["System.WorkItemType"].sanitize()}`,
+        {
+          scopeType: "user",
+          defaultValue: dummy as Models.WorkItemTypeFieldInstance[]
         }
-        document.getElementById("workitems").innerHTML = "";
+      )
+      .then(
+        (data: Models.WorkItemTypeFieldInstance[]) =>
+          data.length > 0 ? data : dummy
+      );
+  });
+}
+
+function getHistory(workItem: Models.WorkItem) {
+  if (vssContext.account.name === "TEAM FOUNDATION") {
+    return client.getHistory(workItem.id).then(comments => {
+      return comments.map(comment => {
+        return {
+          revisedBy: comment.revisedBy,
+          revisedDate: comment.revisedDate,
+          revision: comment.rev,
+          text: comment.value
+        } as Models.WorkItemComment;
+      });
     });
+  }
+
+  return client.getComments(workItem.id).then(comments => comments.comments);
+}
+
+function prepare(workItems: Models.WorkItem[]) {
+  return workItems.map(item => {
+    return Q.all([getFields(item), getHistory(item), getWorkItemFields()])
+      .then(results => {
+        return results;
+      })
+      .spread(
+        (
+          fields: Models.WorkItemTypeFieldInstance[],
+          history: Models.WorkItemComment[],
+          allFields: Models.WorkItemField[]
+        ) => {
+          let insertText =
+            `<div class="item"><h2>${item.fields["System.WorkItemType"]} ` +
+            `${item.id} - ${item.fields["System.Title"]}</h2>`;
+          fields.forEach(field => {
+            const fieldRef = allFields.filter(
+              f => f.referenceName === field.referenceName
+            )[0];
+            if (item.fields[field.referenceName]) {
+              if (fieldRef.type) {
+                switch (fieldRef.type) {
+                  case Models.FieldType.DateTime:
+                    if (
+                      moment(item.fields[field.referenceName]).diff(
+                        moment(),
+                        "years"
+                      ) < 1000
+                    ) {
+                      insertText += `<p><b>${field.name}:</b> ${moment(
+                        item.fields[field.referenceName]
+                      ).format(localeTime)}</p>`;
+                    }
+                    break;
+                  case Models.FieldType.Html:
+                    insertText += `<p><b>${field.name}:</b> ${item.fields[
+                      field.referenceName
+                    ].htmlize()}</p>`;
+                    break;
+                  case Models.FieldType.History:
+                    if (history.length > 0) {
+                      insertText += `<p><b>${field.name}</b></p>`;
+                      history.forEach(comment => {
+                        insertText += `<div class="history"><b>${moment(
+                          comment.revisedDate
+                        ).format(
+                          localeTime
+                        )} ${comment.revisedBy.name.substring(
+                          0,
+                          comment.revisedBy.name.indexOf("<") - 1
+                        )}:</b><br> ${comment.text}</div>`;
+                      });
+                    }
+                    break;
+                  default:
+                    insertText += `<p><b>${field.name}:</b> ${item.fields[
+                      field.referenceName
+                    ]}</p>`;
+                    break;
+                }
+              } else {
+                insertText += `<p><b>${field.name}:</b> ${item.fields[
+                  field.referenceName
+                ]}</p>`;
+              }
+            }
+          });
+          insertText += "</div>";
+          return insertText;
+        }
+      );
+  });
 }
 
 // VSTS/2017
-VSS.register(`${extensionContext.publisherId}.${extensionContext.extensionId}.print-work-item`, printWorkItems);
-VSS.register(`${extensionContext.publisherId}.${extensionContext.extensionId}.print-query-toolbar`, printQueryToolbar);
-VSS.register(`${extensionContext.publisherId}.${extensionContext.extensionId}.print-query-menu`, printQueryToolbar);
+VSS.register(
+  `${extensionContext.publisherId}.${extensionContext.extensionId}.print-work-item`,
+  printWorkItems
+);
+VSS.register(
+  `${extensionContext.publisherId}.${extensionContext.extensionId}.print-query-toolbar`,
+  printQueryToolbar
+);
+VSS.register(
+  `${extensionContext.publisherId}.${extensionContext.extensionId}.print-query-menu`,
+  printQueryToolbar
+);
 
 // 2015
 VSS.register(`print-work-item`, printWorkItems);
